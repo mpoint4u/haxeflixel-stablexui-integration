5f176126e86055009a20a458a42d3cf7cc7442a0
Marcus Pointner <marcus.pointner@gmail.com>
2014-08-04 21:58:06 +0300

    experimenting with the Main class
    checking what needs to get added where ...

--- source/Main.hx
+++ source/Main.hx
@@ -10,24 +10,44 @@ import ru.stablex.ui.UIBuilder;
  * @author Zaphod
  */
 
-class Main extends ru.stablex.ui.widgets.Widget{
-  //callback to create alert popups
-  static public var alert : Dynamic->ru.stablex.ui.widgets.Floating;
-        
-	static function main() 
+class Main extends ru.stablex.ui.widgets.Widget {
+	
+	//callback to create alert popups
+	static public var alert : Dynamic->ru.stablex.ui.widgets.Floating;
+    
+	
+	// entry point
+	public static function main() 
 	{
 		new Main();
 	}
+
+	// constructor
+	public function new() {
+		super();
+
+    //var demo:TileMapDemo = new TileMapDemo();
+	var demo:ScaleModeDemo = new ScaleModeDemo();
 	
-	public function setupUi_HelloWorld()
-  {
+    demo.addEventListener(Event.ADDED_TO_STAGE, function(e:Event){
+      //setupUi_HelloWorld();
+      setupUi_ComplexGUI();
+    });
+     
+    //addChild(demo);    // does not work, gives a black screen ... 
+
+		Lib.current.addChild(demo);
+	}
+	
+	
+/*	public function setupUi_HelloWorld() {
       // Simplest example 
       UIBuilder.init();
       Lib.current.stage.addChild( UIBuilder.buildFn('source/hello_world.xml')() );
-  }
+  }*/
+  
 
-	public function setupUi_ComplexGUI()
-  {
+	public function setupUi_ComplexGUI() {
     //register main class to access it's methods and properties in xml
     UIBuilder.regClass('Main');
 
@@ -43,23 +63,10 @@ class Main extends ru.stablex.ui.widgets.Widget{
     //Create our UI
     var ui = UIBuilder.buildFn('ui/index.xml')();
     ui.show();
+	
+	//Lib.current.addChild(ui);
     Lib.current.stage.addChild(ui);
+	//addChild(ui);
   }
 
-	public function new() 
-	{
-		super();
-
-    //var demo:TileMapDemo = new TileMapDemo();
-	var demo:ScaleModeDemo = new ScaleModeDemo();
-	
-    demo.addEventListener(Event.ADDED_TO_STAGE, function(e:Event){
-      //setupUi_HelloWorld();
-      setupUi_ComplexGUI();
-    });
-     
-    //addChild(demo);
-
-		Lib.current.addChild(demo);
-	}
 }
